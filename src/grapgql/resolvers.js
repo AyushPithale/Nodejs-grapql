60190ed5bedde86e3c9e4d026ec2b9425397ff7a
Ayush Pithale <86419395+AyushPithale@users.noreply.github.com>
2026-01-08 12:07:37 +0000

    Implement product mutations and update GraphQL schema; add .gitignore

--- src/grapgql/resolvers.js
+++ src/grapgql/resolvers.js
@@ -4,8 +4,53 @@ const resolvers = {
   Query: {
     products: () => PRODUCTS,
     product: (_, { id }) =>
-      PRODUCTS.find((product) => product.id === Number(id)),
+      PRODUCTS.find((product) => product.id == Number(id)),
   },
+
+  Mutation: {
+    createProduct: (_, {title, category, price, inStock}) => {
+      console.log("craetePRoduct",{title, category, price, inStock} )
+       const newProduct = {
+        id : String(PRODUCTS.length + 1),
+        title,
+        category,
+        price,
+        inStock
+       }
+
+       PRODUCTS.push(newProduct)
+       return newProduct
+    },
+
+    deleteProduct: (_, {id}) => {
+      const productIndex = PRODUCTS.findIndex((product) => product.id === id)
+      console.log("deletedProductIndex", productIndex)
+      if(productIndex === -1)  return false
+      
+      PRODUCTS.splice(productIndex, 1)
+
+      return true
+
+    },
+
+
+    updateProduct: (_, {id, ...updates}) => {
+     const productIndex = PRODUCTS.findIndex((product) => product.id === id)
+      console.log("updateProductNndex", productIndex)
+        if(productIndex === -1)  return null
+
+      
+
+        const updatedProduct = {
+          ...PRODUCTS[productIndex],
+          ...updates
+        }
+
+        PRODUCTS[productIndex] = updatedProduct
+        return updatedProduct
+    }
+  }
 };
+
 
 module.exports = resolvers;
